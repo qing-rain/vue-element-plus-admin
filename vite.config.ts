1cac792a7194ed0814e91d6a5c1ab3cb048aeab6
qingrain <353752459@qq.com>
2021-10-16 03:26:21 +0800

    add vite ConfigEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import type { UserConfig, ConfigEnv } from 'vite';
+import { resolve } from 'path';
+import { loadEnv } from 'vite';
+import { wrapperEnv } from './build/utils';
 
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ command, mode }:ConfigEnv):UserConfig=>{
+  const root = process.cwd();
+
+  const env = loadEnv(mode, root);
+
+  // The boolean type read by loadEnv is a string. This function can be converted to boolean type
+  const viteEnv = wrapperEnv(env);
+
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
+
+  const isBuild = command === 'build';
+  return {
+    
+  }
+}
