6a93031adf95da31651dbc26c5a2fa26f0693f1c
qingrain <353752459@qq.com>
2021-10-19 00:51:55 +0800

    add viteconfig return

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,75 @@
 import type { UserConfig, ConfigEnv } from 'vite';
+import pkg from './package.json'
+import moment from 'moment';
 import { resolve } from 'path';
 import { loadEnv } from 'vite';
 import { wrapperEnv } from './build/utils';
+import { createProxy } from './build/vite/proxy';
+import { generateModifyVars } from './build/generate/generateModifyVars'
+import { OUTPUT_DIR } from './build/constant';
 
 function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir);
 }
+const { dependencies, devDependencies, name, version } = pkg;
+
+const __APP_INFO__ = {
+  pkg: { dependencies, devDependencies, name, version },
+  lastBuildTime: moment().format('YYYY-MM-DD HH:mm:ss'),
+};
 // https://vitejs.dev/config/
-export default ({ command, mode }:ConfigEnv):UserConfig=>{
+export default ({ command, mode }: ConfigEnv): UserConfig => {
   const root = process.cwd();
 
   const env = loadEnv(mode, root);
 
-  // The boolean type read by loadEnv is a string. This function can be converted to boolean type
   const viteEnv = wrapperEnv(env);
 
   const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
 
   const isBuild = command === 'build';
   return {
-    
+    base: VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias: [
+        // /@/xxxx => src/xxxx
+        {
+          find: /\/@\//,
+          replacement: pathResolve('src') + '/',
+        },
+        // /#/xxxx => types/xxxx
+        {
+          find: /\/#\//,
+          replacement: pathResolve('types') + '/',
+        },
+      ],
+    },
+    server: {
+      host: true,
+      port: VITE_PORT,
+      proxy: createProxy(VITE_PROXY),
+    },
+    build: {
+      target: 'es2015',
+      outDir: OUTPUT_DIR,
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          drop_console: VITE_DROP_CONSOLE,
+        },
+      },
+      brotliSize: false,
+      chunkSizeWarningLimit: 2000,
+    },
+    define: {
+      __INTLIFY_PROD_DEVTOOLS__: false,
+      __APP_INFO__: JSON.stringify(__APP_INFO__),
+    },
+    css: {
+      preprocessorOptions: {
+        additionalData: generateModifyVars()
+      },
+    },
   }
 }
