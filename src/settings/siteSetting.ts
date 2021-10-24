ba8f3607be1c3ae928fd919c48e965dacabce80f
qingrain <353752459@qq.com>
2021-10-24 22:48:45 +0800

    增加项目默认配置

---
+++ src/settings/siteSetting.ts
@@ -0,0 +1,8 @@
+// github repo url
+export const GITHUB_URL = 'https://github.com/qing-rain/vue-element-plus-admin';
+
+// vue-vben-admin-next-doc
+export const DOC_URL = 'https://www.qingrain.cn/';
+
+// site url
+export const SITE_URL = 'https://www.qingrain.cn';
